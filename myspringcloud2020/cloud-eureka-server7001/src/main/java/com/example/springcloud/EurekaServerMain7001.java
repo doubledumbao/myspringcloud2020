ff9625b82a35f283f49463652358cd9b61bb955b
doubledumbao <zhaoran8775@aliyun.com>
2020-05-20 23:25:40 +0800

    eureka

--- myspringcloud2020/cloud-eureka-server7001/src/main/java/com/example/springcloud/EurekaServerMain7001.java
+++ myspringcloud2020/cloud-eureka-server7001/src/main/java/com/example/springcloud/EurekaServerMain7001.java
@@ -2,15 +2,12 @@ package com.example.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-/**
- * @author doubledumbao
- */
 @SpringBootApplication
-@EnableEurekaClient
-public class PaymentMain8001 {
+@EnableEurekaServer
+public class EurekaServerMain7001 {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class,args);
+        SpringApplication.run(EurekaServerMain7001.class,args);
     }
 }
